989705e672036c2712d3496220058f1bad27ade4
krutikasawarkar <krutikasawarkar21@gmail.com>
2021-02-10 16:57:14 +0530

    Structured the project.

---
+++ AniketPieShop/Controllers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using AniketPieShop.Models;
+using AniketPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AniketPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        // GET: /<controller>/
+        public IActionResult List()
+        {
+            PiesListViewModel piesListViewModel = new PiesListViewModel();
+            piesListViewModel.Pies = _pieRepository.AllPies;
+
+            piesListViewModel.CurrentCategory = "Cheese cakes";
+            return View(piesListViewModel);
+        }
+    }
+}
---
+++ AniketPieShop/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AniketPieShop.Models;
+using System.Collections.Generic;
+
+namespace AniketPieShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
